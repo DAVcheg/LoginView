b615a7b022a8d2eb2c3af2c9552bbb71e37f512c
Aleksey Dergunov <davcheg@me.com>
2022-01-24 16:05:23 +1000

    Basic Functions Provided

--- LoginView/LoginViewController.swift
+++ LoginView/LoginViewController.swift
@@ -12,8 +12,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var userNameValueTF: UITextField!
     @IBOutlet var passwordValueTF: UITextField!
     
-    private var userNameSession = "User"
-    private let namePasswordArray = ["User" : "Password"]
+    private var userNameSession = ""
+    private let namePasswordArray = ["User" : "1234"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +28,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func forgotPasswordButton() {
-        showAlert(with: "", and: "Your password is Password")
+        showAlert(with: "", and: "Your password is 1234")
     }
     
     @IBAction func logInButton() {
@@ -76,77 +76,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
         passwordValueTF.becomeFirstResponder()
     } else {
         passwordValueTF.resignFirstResponder()
+        logInButton()
         return true;
     }
     return false
- }
-
+    }
 }
-//    extension UITextField {
-//        class func connectFields(fields:[UITextField]) -> Void {
-//            guard let last = fields.last else {
-//                return
-//            }
-//            for i in 0 ..< fields.count - 1 {
-//                fields[i].addTarget(fields[i+1], action: (UIResponder.becomeFirstResponder), for: .editingDidEndOnExit)
-//            }
-//            last.returnKeyType = .go
-//            last.addTarget(last, action: (UIResponder.resignFirstResponder), for: .editingDidEndOnExit)
-//        }
-//    }
-
-//            while userNameValueTF.text != userName || passwordValueTF.text != password {
-//                showAlert(with: "Oops!", and: "Your name or password is wrong")
-//                print("Your name or password is wrong")
-//                break
-//            }
-            //            guard (userNameValueTF.text == userName) || (passwordValueTF.text == password) else {
-            //                showAlert(with: "Oops!", and: "Your name or password is wrong")
-            //                print("Your name or password is wrong")
-            //                return
-            //            }
-
-//        if userNameValueTF.text != nil, userNameValueTF.text?.count != 0 {
-//            for (userName, _) in namePasswordArray {
-//                if userNameValueTF.text == userName {
-//                    continue
-//                }
-//            }
-//        } else {
-//            let alertController = UIAlertController(
-//                title: "",
-//                message: "Enter your name",
-//                preferredStyle:.alert)
-//            let actionOK = UIAlertAction(title: "OK", style: .default, handler: nil)
-//            alertController.addAction(actionOK)
-//            self.present(alertController, animated: true, completion: nil)
-//        }
-//        if passwordValueTF.text != nil, passwordValueTF.text?.count != 0 {
-//            print("enjoy pass")
-//        } else {
-//            let alertController = UIAlertController(
-//                title: "",
-//                message: "Enter your password",
-//                preferredStyle:.alert)
-//            let actionOK = UIAlertAction(title: "OK", style: .default, handler: nil)
-//            alertController.addAction(actionOK)
-//            self.present(alertController, animated: true, completion: nil)
-//
-        
-        
-//            if passwordValueTF.text != nil, passwordValueTF.text?.count != 0 {
-//                _ = UIAlertController(
-//                    title: "",
-//                    message: "Enter your password",
-//                    preferredStyle:.alert)
-    
-        
-//        for (userName, password) in namePasswordArray {
-//           if userNameValueTF != nill
-//
-//            if userNameValueTF.text ?? "" == userName ?? "" {
-//                print("User Name is true")
-                
-//                self.navigationController!.pushViewController(self.storyboard?.instantiateViewC‌​ontrollerWithIdentifier("welcomeViewController") as! UIViewController, animated: true)
-
-  
